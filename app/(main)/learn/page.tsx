1a83f02b1868f9e78389d610fd4b094b4f5995c2
NickKalchev <n.kalchev1711@gmail.com>
2024-06-12 21:02:27 +0300

    Added courses, server actions and queries

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,10 +1,19 @@
 import FeedWrapper from "@/components/FeedWrapper";
-import Header from "@/components/Header";
 import StickyWrapper from "@/components/StickyWrapper";
 import LearnHeader from "./LearnHeader";
 import UserProgress from "@/components/UserProgress";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 
-function LearnPage() {
+async function LearnPage() {
+    const userProgressData = getUserProgress();
+
+    const [userProgress] = await Promise.all([userProgressData]);
+
+    if(!userProgress || !userProgress.activeCourse) {
+        redirect("/courses");
+    };
+
     return (
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
